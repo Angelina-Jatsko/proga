4f9f7f07da5a925f7698af29f417dbeaadc1e3ca
Angelina-Jatsko <angelina.8709193@gmail.com>
2023-05-30 18:01:34 +0300

    proga

---
+++ lab3.cpp
@@ -0,0 +1,116 @@
+﻿/*Разработать класс Date для работы с датами; В классе должно быть
+три поля: год, месяц и день. Для представления месяцев определить в классе
+перечислимый тип month. Перегрузить операцию operator+ для вычисления
+даты через заданное количество дней. Перегрузить операции инкремента
+operator++ и декремента operator-- для вычисления следующей и предыдущей
+дат.*/
+#include <iostream>
+#include <string>
+
+class Date {
+public:
+    enum Month { january = 1, february, march, april, may, june, july, august, september, october, november, december };
+    Date(int y, Month m, int d) : year(y), month(m), day(d) {}
+
+    void addDays(int n);
+    void printDate() const;
+
+    Date& operator++();
+    Date& operator--();
+    Date operator+(int n) const;
+
+private:
+    int year;
+    Month month;
+    int day;
+};
+
+Date Date::operator+(int n) const {
+    Date res(*this);
+    res.addDays(n);
+    return res;
+}
+
+Date& Date::operator++() {
+    addDays(1);
+    return *this;
+}
+
+Date& Date::operator--() {
+    addDays(-1);
+    return *this;
+}
+
+void Date::printDate() const {
+    std::cout << year << " " << month << " " << day << '\n';
+}
+
+void Date::addDays(int n) {
+    bool added_day;
+    if (n > 0) {
+        added_day = true;
+    }
+    else {
+        added_day = false;
+        n = -n;
+    }
+
+    while (n > 0) {
+        int days_in_month;
+        switch (month) {
+        case january: case march: case may: case july: case august: case october: case december:
+            days_in_month = 31;
+            break;
+        case april: case june: case september: case november:
+            days_in_month = 30;
+            break;
+        case february:
+            days_in_month = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? 29 : 28;
+            break;
+        }
+
+        if (added_day) {
+            day++;
+            if (day > days_in_month) {
+                day = 1;
+                month = static_cast<Date::Month>((month % 12) + 1);
+                if (month == january) {
+                    year++;
+                }
+            }
+        }
+        else {
+            day--;
+            if (day < 1) {
+                month = static_cast<Date::Month>((month + 10) % 12 + 1);
+                if (month == december) {
+                    year--;
+                }
+                day = 31;
+                if (month == april || month == june || month == september || month == november) {
+                    day = 30;
+                }
+                else if (month == february) {
+                    day = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? 29 : 28;
+                }
+            }
+        }
+        n--;
+    }
+}
+
+int main() {
+    Date d(2021, Date::january, 1);
+    d.printDate();
+
+    d = d + 100;
+    d.printDate();
+
+    ++d;
+    d.printDate();
+
+    --d;
+    d.printDate();
+
+    return 0;
+}
---
+++ laba5.cpp
@@ -0,0 +1,51 @@
+﻿/*Класс "Прямоугольный параллелепипед".Создать объект класса, моделирующий параллелепипед, сделанный из пластмассы, 
+с параметрами : а, b - стороны прямоугольника - основания, H - высота параллелепипеда.Вычислить полную поверхность
+и объем исходного объекта и нового объекта, полученного удвоением параметров исходного параллелепипеда.*/
+
+#include <iostream>
+
+using namespace std;
+
+class Parallelepiped {
+private:
+    int a;
+    int b;
+    int h;
+
+public:
+    Parallelepiped(int a, int b, int h) {
+        this->a = a;
+        this->b = b;
+        this->h = h;
+    }
+
+    int getA() const {
+        return a;
+    }
+
+    int getB() const {
+        return b;
+    }
+
+    int getH() const {
+        return h;
+    }
+
+    int volume() const {
+        return a * b * h;
+    }
+
+    int surfaceArea() const {
+        return 2 * (a * b + a * h + b * h);
+    }
+};
+
+int main() {
+    Parallelepiped p(3, 4, 5);
+    cout << "Initial Parallelepiped - Volume: " << p.volume() << ", Surface Area: " << p.surfaceArea() << endl;
+
+    Parallelepiped p2(2 * p.getA(), 2 * p.getB(), 2 * p.getH());
+    cout << "Doubled Parallelepiped - Volume: " << p2.volume() << ", Surface Area: " << p2.surfaceArea() << endl;
+
+    return 0;
+}
